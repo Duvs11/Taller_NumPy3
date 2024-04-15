30d7681dbd73e6c3dd798ac88786b123c7163af4
Duvan Ramirez <solduvan11@gmail.com>
2024-04-15 15:13:02 -0500

    v0.02

--- programa_.py
+++ programa_.py
@@ -3,7 +3,19 @@
 import numpy as np
 
 # Crear un ndarray de 2 dimensiones a partir de una lista de listas
-
 A=np.array([1,5],[7,9])
 B=np.array([2,0],[1,3])
+
+# Producto punto entre ndarrays
+C=np.dot(A,B)
+print(C)
+
+# Solucion de un sistema de ecuaciones con NumPy
+
+m_solucion=np.array([5,17])
+
+# Linalg es un algoritmo de solucón lineal
+
+m=np.linalg.solve(A,m_solucion)
+print(m)
 
